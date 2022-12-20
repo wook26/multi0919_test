ceab1dbab752ef531d1b51feabaa9527838fb9d7
leesi <leesi@114.200.175.35>
2022-12-20 12:15:47 +0900

    1.1 / 로그인부 부분 변경 / 이시욱 / 2022/12/20 12:15
    -로그인 부분변경

--- GIT_TEST/src/main/java/com/multi/test/HomeController.java
+++ GIT_TEST/src/main/java/com/multi/test/HomeController.java
@@ -23,7 +23,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		System.out.println("Version 1.0");
+		System.out.println("Version 1.1");
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
--- GIT_TEST/src/main/webapp/WEB-INF/views/home.jsp
+++ GIT_TEST/src/main/webapp/WEB-INF/views/home.jsp
@@ -10,6 +10,6 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
-<p> GIT Version 1.0 </p>
+<p> GIT Version 1.1 </p>
 </body>
 </html>
